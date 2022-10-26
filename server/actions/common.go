6111084f2fb7bd4acfc8cdba3d19b8178b481775
Herman Karlsson <herkarlsson@gmail.com>
2022-10-26 02:27:04 +0200

    Remove unused file

--- server/actions/common.go
+++
@@ -1,7 +0,0 @@
-package actions
-
-// All functions in this file should be passed a db as a parameter,
-// in order to avoid deadlocks, since all functions that should
-// call functions in this file should fetch the database connection
-// themself
-
