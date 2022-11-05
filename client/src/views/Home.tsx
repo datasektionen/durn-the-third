b5e97ceddbcd50499aa3733c119989f9f9d34a42
Herman Karlsson <herkarlsson@gmail.com>
2022-11-05 12:58:49 +0100

    add election fetching

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Header } from "methone";
-import { Grid, Skeleton, Container } from "@mantine/core";
+import { Grid, Skeleton, Container, createStyles } from "@mantine/core";
 import { electionMock, DisplayElectionInfo, Election } from "../components/Election";
+import axios from "axios";
+import useAuthorization from "../hooks/useAuthorization";
 
 const Info: React.FC = () => {
   return <Skeleton height={300} animate={false}> </Skeleton>
@@ -13,9 +15,18 @@ const getElections = (): Election[] =>  {
 
 export const Home: React.FC = (props) => {
   const [elections, setElections] = useState<Election[]>([]);
+  const {authHeader} = useAuthorization()
   useEffect(() => {
-    setElections(getElections());
-  });
+    axios(`/api/elections/public`, {
+      headers: authHeader
+    }).then((res) => {
+      console.log(res.data)
+      setElections(res.data)
+    }).catch((reason) => {
+      console.log(reason.message)
+      setElections(getElections());
+    })
+  }, [authHeader]);
 
   return (<>
     <Header title="Hem" />
