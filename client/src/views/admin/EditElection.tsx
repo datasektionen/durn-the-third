5b9bc95a4d57db645b0ddbdcbf76ffd7cfdc120b
Herman Karlsson <herkarlsson@gmail.com>
2022-11-06 16:49:38 +0100

    add file for election editing

---
+++ client/src/views/admin/EditElection.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import {
+  useParams,
+} from "react-router-dom";
+
+const EditElection: React.FC = () => {
+  
+  return <></>
+}
+
+
+export default EditElection
