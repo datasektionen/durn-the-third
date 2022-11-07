222c44e007b2e6ec5d598db0101c32229b0fc660
Herman Karlsson <herkarlsson@gmail.com>
2022-11-07 01:07:31 +0100

    Add basic election editing structure

--- client/src/views/admin/EditElection.tsx
+++ client/src/views/admin/EditElection.tsx
@@ -1,11 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import {
   useParams,
 } from "react-router-dom";
+import { Header } from "methone"
+import axios from "axios";
+
+import { Grid, Skeleton, Container } from "@mantine/core";
+import { useForm } from "@mantine/form";
+
+import { Election, Candidate, electionMock } from "../../components/Election";
+import useAuthorization from "../../hooks/useAuthorization";
 
 const EditElection: React.FC = () => {
-  
-  return <></>
+  const electionId = useParams()["id"]
+  const [election, setElection] = useState<Election>(electionMock())
+  const { authHeader } = useAuthorization()
+
+  axios(`/api/election/${electionId}`, {
+    headers: authHeader
+  }).then((res) => {
+    setElection(res.data)
+  })
+
+  const form = useForm({
+    initialValues: {
+      name: election.name,
+      description: election.description,
+      openTime: election.openTime,
+      closeTime: election.closeTime
+    }
+  })
+
+  return <>
+    <Header title="Editing election" />
+    <Container my="md">
+      <Grid>
+        <Grid.Col span={3}>
+          
+        </Grid.Col>
+        <Grid.Col span={9}>
+          <Skeleton height={200} animate={false}/>
+        </Grid.Col>
+      </Grid>
+    </Container>
+  </>
 }
 
 
