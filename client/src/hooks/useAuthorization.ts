9223bb4dc6af2a3c5be541887aaf99629445da88
Herman Karlsson <herkarlsson@gmail.com>
2022-11-10 00:03:34 +0100

    Move authorization code to a place where it is only run once

--- client/src/hooks/useAuthorization.ts
+++ client/src/hooks/useAuthorization.ts
@@ -1,11 +1,8 @@
-import { useEffect } from "react";
-import axios from "axios";
 import { useLocalStorage } from "@mantine/hooks";
-
 
 const useAuthorization = () => {
   const [loggedIn, setLoggedIn, removeLoggedIn] = useLocalStorage({
-    key: "logged-in", defaultValue: false
+    key: "loggedIn", defaultValue: false
   });
   const [user, setUser, removeUser] = useLocalStorage<string>({
     key: "user", defaultValue: ""
@@ -19,30 +16,6 @@ const useAuthorization = () => {
   const [token, setToken, removeToken] = useLocalStorage<string | null>({
     key: "token", defaultValue: null
   })
-  const [prevToken, setPrevToken] = useLocalStorage<string | null>({
-    key: "prevToken", defaultValue: null
-  })
-
-  useEffect(() => {
-    if (token == null) return
-    if (prevToken == token) return
-    const header = { "Authorization": `Bearer ${token}` }
-    axios.get('/api/validate-token', {
-      headers: header
-    }).then(res => {
-      setLoggedIn(true);
-      setUser(res.data.user);
-      setPerms(res.data.perms);
-      setHeader(header)
-      setPrevToken(token)
-    }).catch(() => { // login token invalid, possibly because it has expired
-      removeToken()
-      removeHeader()
-      removeUser()
-      removePerms()
-      removeLoggedIn()
-    })
-  }, [token])
 
   return {
     loggedIn,
