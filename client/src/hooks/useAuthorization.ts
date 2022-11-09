46f6a4085252e420c63c743130a2c3bf12e7a924
Herman Karlsson <herkarlsson@gmail.com>
2022-11-09 02:02:32 +0100

    optimize authorization

--- client/src/hooks/useAuthorization.ts
+++ client/src/hooks/useAuthorization.ts
@@ -1,37 +1,46 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useLocalStorage } from "@mantine/hooks";
+
 
 const useAuthorization = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [user, setUser] = useState("");
-  const [perms, setPerms] = useState<string[]>([]);
-  const [header, setHeader] = useState<object>({})
-  const [token, setToken] = useState("")
+  const [loggedIn, setLoggedIn, removeLoggedIn] = useLocalStorage({
+    key: "logged-in", defaultValue: false
+  });
+  const [user, setUser, removeUser] = useLocalStorage<string>({
+    key: "user", defaultValue: ""
+  });
+  const [perms, setPerms, removePerms] = useLocalStorage<string[]>({
+    key: "perms", defaultValue: []
+  });
+  const [header, setHeader, removeHeader] = useLocalStorage<object>({
+    key: "header", defaultValue: {}
+  })
+  const [token, setToken, removeToken] = useLocalStorage<string | null>({
+    key: "token", defaultValue: null
+  })
+  const [prevToken, setPrevToken] = useLocalStorage<string | null>({
+    key: "prevToken", defaultValue: null
+  })
 
   useEffect(() => {
-    const tempToken = localStorage.getItem("token");
-    if (tempToken === null) {
-      setToken("")
-      return;
-    }
-    if (tempToken != token) {
-      setToken(tempToken)
-    }
-  }, [localStorage])
-
-  useEffect(() => {
-    if (token == "") return;
+    if (token == null) return
+    if (prevToken == token) return
     const header = { "Authorization": `Bearer ${token}` }
     axios.get('/api/validate-token', {
       headers: header
     }).then(res => {
-      console.log(res);
       setLoggedIn(true);
       setUser(res.data.user);
       setPerms(res.data.perms);
       setHeader(header)
+      setPrevToken(token)
     }).catch(() => { // login token invalid, possibly because it has expired
-      localStorage.removeItem("token")
+      removeToken()
+      removeHeader()
+      removeUser()
+      removePerms()
+      removeLoggedIn()
     })
   }, [token])
 
