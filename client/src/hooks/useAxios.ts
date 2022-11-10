67db16c6ab9bfdee9ac5289c52981c5e7db00af4
Herman Karlsson <herkarlsson@gmail.com>
2022-11-10 01:55:01 +0100

    Add axios hook

---
+++ client/src/hooks/useAxios.ts
@@ -0,0 +1,41 @@
+import { useLocalStorage } from "@mantine/hooks";
+import axios from "axios";
+import constants from "../util/constants";
+
+import useAuthorization from "./useAuthorization";
+
+export interface ErrorData {
+  code: number,
+  response: string
+}
+
+export const useApiRequester = () => {
+  const { authHeader } = useAuthorization()
+  return (
+    method: "get" | "put" | "post" | "delete" | "patch",
+    url: string,
+    data: any,
+    onSuccess: (data: any) => void = () => {},
+    onError: (error: ErrorData) => void = () => {},
+    headers: any = {}
+  ) => {
+    console.log(data)
+    axios({
+      method: method,
+      url: url,
+      data: data,
+      headers: {
+        ...authHeader,
+        ...headers,
+      }
+    }).then(({data}) => {
+      onSuccess(data)
+    }).catch(({response}) => {
+      const { status, data } = response
+      onError({
+        code: status,
+        response: data
+      })
+    })
+  }
+}
