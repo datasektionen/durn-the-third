a6eb02ee15e651f2334a9dc347b23f81554689e8
Herman Karlsson <herkarlsson@gmail.com>
2023-03-13 17:10:18 +0100

    add wrapper for data fetching loading display

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,10 +1,40 @@
 import React from "react"
 
 
-const Loading: React.FC = () => {
+export const Loading: React.FC = () => {
 
   return <>
     Loading
+  </>
+}
+
+
+export interface ErrorProps {
+  error: string
+}
+export const Error: React.FC<ErrorProps> = ({ error }) => {
+
+  return <>
+    Error <br/>
+    {error}
+  </>
+}
+
+
+export interface LoadingContainerProps {
+  error: string | null,
+  loading: boolean
+}
+export const LoadingContainer: React.FC<LoadingContainerProps> = ( {
+  loading, error, children
+} ) => {
+  return <>
+    {loading && 
+      <Loading />}
+    {!loading && error &&
+      <Error error={error} />}
+    {!loading && !error && 
+      children}
   </>
 }
 
