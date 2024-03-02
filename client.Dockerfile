04d1a943295b45e92cbee53ab2073b189750e0c0
Herman Karlsson <herkarlsson@gmail.com>
2024-03-02 01:17:53 +0100

    general improvements

--- client.Dockerfile
+++ client.Dockerfile
@@ -1,8 +1,8 @@
-FROM node:19-alpine
+FROM node:16-alpine
 WORKDIR /app
 COPY client/package.json client/package-lock.json /app/
 RUN npm install
 COPY client/ /app/
 EXPOSE 8080
 
-CMD ["npm", "start"]
+CMD ["npm", "run", "start"]
--- client/src/util/funcs.ts
+++ client/src/util/funcs.ts
@@ -3,6 +3,5 @@ export function compareList<T>(a: T[], b:T[]): number {
     if (a[i] < b[i]) return -1
     if (b[i] < a[i]) return 1
   }
-  if (a.length == b.length) return 0
-  return a.length < b.length ? -1 : 1 
+  return a.length - b.length
 }
